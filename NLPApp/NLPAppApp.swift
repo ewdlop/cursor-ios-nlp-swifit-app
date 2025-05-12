6fa12cab0914c8f56cb39e6647f41cb8ca4042a1
ERA <ray810815@gmail.com>
2025-05-12 17:34:33 -0400

    Initial Commit

---
+++ NLPApp/NLPAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NLPAppApp.swift
+//  NLPApp
+//
+//  Created by Raymond Lei on 5/12/25.
+//
+
+import SwiftUI
+
+@main
+struct NLPAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NLPAppTests/NLPAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  NLPAppTests.swift
+//  NLPAppTests
+//
+//  Created by Raymond Lei on 5/12/25.
+//
+
+import Testing
+@testable import NLPApp
+
+struct NLPAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
